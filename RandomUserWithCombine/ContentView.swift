d15cee8d444f1395287de79933307f6573b2b791
Aphaki <kiapha1108@gmail.com>
2022-04-12 00:46:37 +0900

    Initial Commit

---
+++ RandomUserWithCombine/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RandomUserWithCombine
+//
+//  Created by Sy Lee on 2022/04/12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ RandomUserWithCombine/RandomUserWithCombineApp.swift
@@ -0,0 +1,17 @@
+//
+//  RandomUserWithCombineApp.swift
+//  RandomUserWithCombine
+//
+//  Created by Sy Lee on 2022/04/12.
+//
+
+import SwiftUI
+
+@main
+struct RandomUserWithCombineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
